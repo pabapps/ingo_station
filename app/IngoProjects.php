66141a346d5f6cf644637fc7471db8daf311b7ce
Raihan Zaman <raihan.zaman@amadeyr.org>
2017-08-13 11:14:29 +0600

    created the model of the ingo_project table

---
+++ app/IngoProjects.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class IngoProjects extends Model
+{
+    protected $table = 'ingo_projects';
+    public $timestamps = false;
+    protected $primaryKey = 'id';
+}
