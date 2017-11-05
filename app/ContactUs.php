41422c060351034ad489cda4c97175615a9e3245
Raihan <raihan.zaman@practicalaction.org.bd>
2017-11-05 10:53:27 +0600

    working on the model of the contactUS

---
+++ app/ContactUs.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContactUs extends Model
+{
+    protected $table = 'contactus';
+    public $timestamps = false;
+    protected $primaryKey = 'id';
+}
